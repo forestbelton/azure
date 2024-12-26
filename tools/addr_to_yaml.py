f7cedb48e983ad29b3064534d52ef7f698ec7bbb
Forest Belton <forest@homolo.gy>
2024-12-26 16:58:39 -0500

    Factor out address to splat YAML conversion

--- tools/addr_to_yaml.py
+++ tools/addr_to_yaml.py
@@ -1,26 +1,33 @@
 import argparse
 import re
+from typing import Optional
 
 SYMBOL_ADDR_PATTERN = re.compile(r"^(\w+) = 0x([a-fA-F0-9]+); .*$")
 
 
-def addr_to_yaml(filename: str) -> None:
+def convert_line(line: str) -> Optional[str]:
+    m = SYMBOL_ADDR_PATTERN.match(line)
+    if m is None:
+        return
+    name = m.group(1)
+    addr = int(m.group(2), base=16) - 0x8002D000 + 0x800
+    assert addr >= 0
+    return f"      - [{hex(addr)}, asm] # {name}"
+
+
+def convert_file(filename: str) -> None:
     with open(filename, "r") as f:
         for line in f:
-            m = SYMBOL_ADDR_PATTERN.match(line)
-            if m is None:
-                continue
-            name = m.group(1)
-            addr = int(m.group(2), base=16) - 0x8002D000 + 0x800
-            assert addr >= 0
-            print(f"      - [{hex(addr)}, asm] # {name}")
+            yaml_line = convert_line(line)
+            if yaml_line is not None:
+                print(yaml_line)
 
 
 def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument("file")
     args = parser.parse_args()
-    addr_to_yaml(args.file)
+    convert_file(args.file)
 
 
 if __name__ == "__main__":
